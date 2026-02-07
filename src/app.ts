f1809ea40f6093c9aa2d4c1d395b39ab5021f9dd
Sumanpreet SIngh <ssingh28@rrc.ca>
2026-02-07 15:43:15 -0500

    Updated app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import morgan from "morgan";
 import healthRoutes from "./api/v1/routes/healthRoutes";
+import ticketRoutes from "./api/v1/routes/ticketRoutes";
 
 const app: Express = express();
 
@@ -9,7 +10,9 @@ app.use(morgan("combined"));
 
 // Parse JSON bodies
 app.use(express.json());
+
 app.use("/api/v1", healthRoutes);
+app.use("/api/v1", ticketRoutes);
 
 // Export the app
 export default app;
